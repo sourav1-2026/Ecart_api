258006533481225a9e3d0b06de6aad812c4cbcc3
sourav <souravkshaw2019@gmail.com>
2023-01-29 21:14:08 +0530

    model created

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+from .models import Category,Product,Order,OrderItem
+
+@admin.register(Category)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description','price']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'created_at']
+
+
+@admin.register(OrderItem)
+class OrdeItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'quantity', 'price','product']
--- api/models.py
+++ api/models.py
@@ -3,3 +3,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name= models.CharField(max_length=255)
+    description= models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    image=models.ImageField()
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Order(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.IntegerField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
