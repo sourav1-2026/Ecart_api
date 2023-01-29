55f1e07e6b27dd9fb698b268e306a885dbccdd88
sourav <souravkshaw2019@gmail.com>
2023-01-29 22:26:37 +0530

    seralizer of each model created

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=10, decimal_places=2)
-    image=models.ImageField()
+    image=models.ImageField(upload_to='shope')
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
---
+++ api/serializer.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields = '__all__'
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name can not be blank")
+        return value
+
+    def validate_description(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name can not be blank")
+        return value
+    
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields = '__all__'
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name can not be blank")
+        return value
+    
+    def validate_description(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name can not be blank")
+        return value
+    
+    
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number")
+        return value
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=OrderItem
+        fields = '__all__'
+    
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number")
+        return value
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number")
+        return value
